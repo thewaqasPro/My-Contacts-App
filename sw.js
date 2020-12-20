24e625a4f0c7e44e63de5797c68b84eded5b0d6f
Muhammad Waqas <waqas.attary@gmail.com>
2020-12-20 15:48:07 +0500

    done

--- sw.js
+++ sw.js
@@ -40,16 +40,16 @@ self.addEventListener('activate', e => {
     console.log('avtivate')
 })
 
-// self.addEventListener('fetch', e => {
-//     e.respondWith(
-//         caches.match(e.request).then(staticRes => {
-//             return staticRes || fetch(e.request).then(dynamicRes => {
-//                 return caches.open(dynamicCache).then(cache => {
-//                     cache.put(e.request.url, dynamicRes.clone())
-//                     limitNumCache(dynamicCache, 3)
-//                     return dynamicRes
-//                 })
-//             })
-//         }).catch( ()=>caches.match('/pages/fallback.html'))
-//     )
-// })
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(staticRes => {
+            return staticRes || fetch(e.request).then(dynamicRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(e.request.url, dynamicRes.clone())
+                    limitNumCache(dynamicCache, 3)
+                    return dynamicRes
+                })
+            })
+        }).catch( ()=>caches.match('/pages/fallback.html'))
+    )
+})
