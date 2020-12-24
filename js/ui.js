3bc1903c963de2903f0011140a1a52894e0b5fa5
Muhammad Waqas <waqas.attary@gmail.com>
2020-12-24 12:56:49 +0500

    Commit

--- js/ui.js
+++ js/ui.js
@@ -52,6 +52,21 @@ autocompleteField.addEventListener('focus', () => {
 
 
 
+const renderContacts = (data, id) => {
+    const html = `
+    <li class="contact collection-item avatar" data-id=${id}>
+        <i class="material-icons circle blue">account_box</i>
+        Name: <span class="name">${data.name}</span><br>
+        Phone:<span class="phone">${data.phone}</span>
+        <div class="secondary-content" data-id=${id}>
+            <i class="material-icons modal-trigger" href="#update_contact_modal">edit</i>
+            <i class="material-icons favorite-icon">${data.favorite ? 'star' : 'star_border'}</i>
+            <i class="material-icons">delete_outline</i>
+        </div>
+    </li>
+    `
+    contacts.innerHTML += html
+}
 
 
 
